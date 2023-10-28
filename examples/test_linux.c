64f995ff0c7e91570f85ee695696febbe5d952da
Rekord <1324596506@qq.com>
2023-10-28 13:29:52 +0000

    chore: add more custom test example

--- examples/test_linux.c
+++ examples/test_linux.c
@@ -6,6 +6,7 @@
 MultiTimer timer1;
 MultiTimer timer2;
 MultiTimer timer3;
+MultiTimer timer4;
 
 uint64_t PlatformTicksGetFunc(void)
 {
@@ -31,6 +32,20 @@ void exampleTimer3Callback(MultiTimer* timer, void *userData)
     MultiTimerStart(timer, 4567, exampleTimer3Callback, userData);
 }
 
+typedef struct CustomUserData {
+    int count;
+    char* str;
+} CustomUserData;
+void exampleTimer4Callback(MultiTimer* timer, void *userData)
+{
+    CustomUserData* customUserData = (CustomUserData*)userData;
+    customUserData->count--;
+    printf("[%012ld] Timer:%p callback-> %s.\r\n", PlatformTicksGetFunc(), timer, customUserData->str);
+    if (customUserData->count > 0) {
+        MultiTimerStart(timer, 2000, exampleTimer4Callback, customUserData);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     MultiTimerInstall(PlatformTicksGetFunc);
@@ -38,6 +53,11 @@ int main(int argc, char *argv[])
     MultiTimerStart(&timer1, 1000, exampleTimer1Callback, "1000ms CYCLE timer");
     MultiTimerStart(&timer2, 5000, exampleTimer2Callback, "5000ms ONCE timer");
     MultiTimerStart(&timer3, 3456, exampleTimer3Callback, "3456ms delay start, 4567ms CYCLE timer");
+    CustomUserData customUserData = {
+        .count = 3,
+        .str = "2000ms 3 timer"
+    };
+    MultiTimerStart(&timer4, 2000, exampleTimer4Callback, &customUserData);
 
     while (1) {
         MultiTimerYield();
